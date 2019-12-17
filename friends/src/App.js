25308e6ca1d906183a5d61137171e842f047cd51
wchamber01 <williamchambers@gmail.com>
2019-12-17 17:38:37 -0500

    setup folders and app.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
 import "./App.css";
+
+import Login from "./components/Login";
+import Friends from "./components/Friends";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My Friends App</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <Link to="/login">Login</Link>
+            <Link to="/protected">Protected Page</Link>
+          </nav>
+          <Switch>
+            <PrivateRoute path="/protected" component={Friends} />
+            <Route path="/login" component={Login} />
+          </Switch>
+          <h1>My Friends App</h1>
+        </header>
+      </div>
+    </Router>
   );
 }
 
